664ce048120c88aec92a638e400cbd77fd5bea17
Yahir Salazar Ortiz <yahir.salazarortiz0113@gmail.com>
2025-09-10 17:48:21 -0600

    Initial Commit

---
+++ a3dificultad/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  a3dificultad
+//
+//  Created by WIN603 on 10/09/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ a3dificultad/a3dificultadApp.swift
@@ -0,0 +1,17 @@
+//
+//  a3dificultadApp.swift
+//  a3dificultad
+//
+//  Created by WIN603 on 10/09/25.
+//
+
+import SwiftUI
+
+@main
+struct a3dificultadApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
