f955016dc61eb3af18d125573b46865ada59ab21
yhrslzr <yahir.salazarortiz0113@gmail.com>
2025-09-16 15:33:11 -0600

    swift act 3 parcial 1 v1.2

--- a3dificultad/ButtonView.swift
+++ a3dificultad/ButtonView.swift
@@ -8,149 +8,157 @@
 import SwiftUI
 
 struct ButtonView: View {
-    @State var level: Int = 0
+    @Binding var level: Int
     var body: some View {
         // Vista Botones
-        ZStack (alignment: .center){
-            // Nivel 1
-            Button(action: {
-                
-                withAnimation(){
-                    level = 1
+        ZStack (alignment: .centerFirstTextBaseline){
+            HStack{
+                // Nivel 1
+                Button(action: {
+                    withAnimation(){
+                        level = 1
+                    }
+                }) {
+                    Text("Brisa dulce")
+                        .padding(10)
                 }
+                .background(Color.blue)
+                .cornerRadius(7)
                 
-            }) {
-                Text("Brisa dulce")
-                    .padding(10)
+                // Nivel 2
+                Button(action: {
+                    
+                    withAnimation(){
+                        level = 2
+                    }
+                    
+                }) {
+                    Text("Paseo Azucarado")
+                        .padding(10)
+                }
+                .background(Color.teal)
+                .cornerRadius(7)
+                
+                // Nivel 3
+                Button(action: {
+                    
+                    withAnimation(){
+                        level = 3
+                    }
+                    
+                }) {
+                    Text("Expedición ácida")
+                        .padding(10)
+                }
+                .background(Color.green)
+                .cornerRadius(7)
+                
+                
+                
             }
-            .background(Color.blue)
-            .cornerRadius(40)
+            .offset(y: 10)
             .foregroundColor(.white)
             
-            // Nivel 2
-            Button(action: {
-                
-                withAnimation(){
-                    level = 2
+            HStack{
+                // Nivel 4
+                Button(action: {
+                    
+                    withAnimation(){
+                        level = 4
+                    }
+                    
+                }) {
+                    Text("Hazaña picapica")
+                        .padding(10)
                 }
+                .background(Color.pink)
+                .cornerRadius(7)
                 
-            }) {
-                Text("Paseo Azucarado")
-                    .padding(10)
+                // Nivel 5
+                Button(action: {
+                    
+                    withAnimation(){
+                        level = 5
+                    }
+                    
+                }) {
+                    Text("Prueba ardiente")
+                        .padding(10)
+                }
+                .background(Color.orange)
+                .cornerRadius(7)
+                
+                // Nivel 6
+                Button(action: {
+                    
+                    withAnimation(){
+                        level = 6
+                    }
+                    
+                }) {
+                    Text("Lucha abrasadora")
+                        .padding(10)
+                }
+                .background(Color.gray)
+                .cornerRadius(7)
+                
             }
-            .background(Color.teal)
-            .cornerRadius(40)
+            .offset(y: 90)
             .foregroundColor(.white)
             
-            // Nivel 3
-            Button(action: {
-                
-                withAnimation(){
-                    level = 3
+            HStack{
+                // Nivel 7
+                Button(action: {
+                    
+                    withAnimation(){
+                        level = 7
+                    }
+                    
+                }) {
+                    Text("Crisis de Magma")
+                        .padding(10)
                 }
+                .background(Color("lvl7"))
+                .cornerRadius(7)
                 
-            }) {
-                Text("Expedición ácida")
-                    .padding(10)
+                // Nivel 8
+                Button(action: {
+                    
+                    withAnimation(){
+                        level = 8
+                    }
+                    
+                }) {
+                    Text("Derritealmas")
+                        .padding(10)
+                }
+                .background(Color.black)
+                .cornerRadius(7)
+                
+                // Nivel 9
+                Button(action: {
+                    
+                    withAnimation(){
+                        level = 9
+                    }
+                    
+                }) {
+                    Text("Derritealmas EX")
+                        .padding(10)
+                }
+                .background(Color("lvl9"))
+                .cornerRadius(7)
             }
-            .background(Color.green)
-            .cornerRadius(40)
+            .offset(y: 170)
             .foregroundColor(.white)
             
-            // Nivel 4
-            Button(action: {
-                
-                withAnimation(){
-                    level = 4
-                }
-                
-            }) {
-                Text("Hazaña picapica")
-                    .padding(10)
-            }
-            .background(Color.pink)
-            .cornerRadius(40)
-            .foregroundColor(.white)
             
-            // Nivel 5
-            Button(action: {
-                
-                withAnimation(){
-                    level = 5
-                }
-                
-            }) {
-                Text("Prueba ardiente")
-                    .padding(10)
-            }
-            .background(Color.orange)
-            .cornerRadius(40)
-            .foregroundColor(.white)
-            
-            // Nivel 6
-            Button(action: {
-                
-                withAnimation(){
-                    level = 6
-                }
-                
-            }) {
-                Text("Lucha abrasadora")
-                    .padding(10)
-            }
-            .background(Color.gray)
-            .cornerRadius(40)
-            .foregroundColor(.white)
-            
-            // Nivel 7
-            Button(action: {
-                
-                withAnimation(){
-                    level = 7
-                }
-                
-            }) {
-                Text("Crisis de Magma")
-                    .padding(10)
-            }
-            .background(Color("lvl7"))
-            .cornerRadius(40)
-            .foregroundColor(.white)
-            
-            // Nivel 8
-            Button(action: {
-                
-                withAnimation(){
-                    level = 8
-                }
-                
-            }) {
-                Text("Derritealmas")
-                    .padding(10)
-            }
-            .background(Color.black)
-            .cornerRadius(40)
-            .foregroundColor(.white)
-            
-            // Nivel 9
-            Button(action: {
-                
-                withAnimation(){
-                    level = 8
-                }
-                
-            }) {
-                Text("Derritealmas EX")
-                    .padding(10)
-            }
-            .background(Color("lvl9"))
-            .cornerRadius(40)
-            .foregroundColor(.white)
-            
-        }.frame(width: .infinity, height: .infinity)
+        }
+        .frame(width: .infinity, height: .infinity)
+        
     }
 }
 
-#Preview {
-    ButtonView(level: 0)
-}
+//#Preview {
+//    ButtonView(level: )
+//}
--- a3dificultad/ContentView.swift
+++ a3dificultad/ContentView.swift
@@ -6,58 +6,134 @@
 //
 
 import SwiftUI
+import AVFoundation
 
 struct ContentView: View {
-    var level: Int = 0
+    
+    @State var level: Int = 0
+    @State private var player: AVAudioPlayer?
+    
     
     var body: some View {
-        VStack {
-            //Título
-            Text("Selecciona tu 'picante'")
-                .font(.title).bold()
-            
-            Spacer()
-            
-            // Vista del Medidor de Dificultad
-            HStack{
-                if level == 0 {
-                    Rectangle()
-                        .fill(Color.red)
-                        .frame(width: .infinity, height: .infinity)
-                        .overlay{
-                            Image("ksa tuc")
-                                .resizable()
-                        }
-                } else if level == 1 {
-                    
-                    Rectangle()
-                        .fill(Color("lvl1"))
-                        .frame(width: .infinity, height: .infinity)
-                        .overlay{
-                            Image("tuclvl1")
-                                .resizable()
-                        }
-                    
-                } else if level == 2 {
-                    Rectangle()
-                        .fill(Color("lvl2"))
-                        .frame(width: .infinity, height: .infinity)
-                        .overlay{
-                            Image("tuclvl2")
-                                .resizable()
-                        }
-                }
+        ZStack (alignment: .top){
+            VStack (alignment: .center) {
                 
+                //Título
+                Text("Selecciona tu 'picante'")
+                    .font(.title).bold()
+                    .onAppear(){
+                        playMusic()
+                    }
+                
+                // Vista del Medidor de Dificultad
+                HStack{
+                    if level == 1
+                    {
+                        Rectangle()
+                            .fill(Color("lvl1"))
+                            .overlay{
+                                Image("tuclvl1")
+                                    .resizable()
+                            }
+                        
+                    } else if level == 2 {
+                        
+                        Rectangle()
+                            .fill(Color("lvl2"))
+                            .overlay{
+                                Image("tuclvl2")
+                                    .resizable()
+                            }
+                        
+                    } else if level == 3 {
+                        Rectangle()
+                            .fill(Color("lvl3"))
+                            .overlay{
+                                Image("tuclvl3")
+                                    .resizable()
+                            }
+                    } else if level == 4 {
+                        Rectangle()
+                            .fill(Color("lvl4"))
+                            .overlay{
+                                Image("tuclvl4")
+                                    .resizable()
+                            }
+                    } else if level == 5 {
+                        Rectangle()
+                            .fill(Color("lvl5"))
+                            .overlay{
+                                Image("tuclvl5")
+                                    .resizable()
+                            }
+                    } else if level == 6 {
+                        Rectangle()
+                            .fill(Color("lvl6"))
+                            .overlay{
+                                Image("tuclvl6")
+                                    .resizable()
+                            }
+                    } else if level == 7 {
+                        Rectangle()
+                            .fill(Color("lvl7"))
+                            .overlay{
+                                Image("tuclvl7")
+                                    .resizable()
+                            }
+                    } else if level == 8 {
+                        Rectangle()
+                            .fill(Color("lvl8"))
+                            .overlay{
+                                Image("tuclvl8")
+                                    .resizable()
+                            }
+                    } else if level == 9 {
+                        Rectangle()
+                            .fill(Color("lvl9"))
+                            .overlay{
+                                Image("tuclvl9")
+                                    .resizable()
+                            }
+                    }
+                    
+                    else {
+                        Rectangle()
+                            .fill(Color.red)
+                            .overlay{
+                                Image("ksatuc")
+                                    .resizable()
+                            }
+                    }
+                }
+                .frame(width: .infinity, height: 200)
+                
+                // Vista de Botones
+                ButtonView(level: $level)
+                
+        
             }
-            
-            Spacer()
-            
-            ButtonView(level: 0)
-            
+            .padding()
+            .offset(y: -60)
         }
-        .padding()
     }
+    
+    // Reproductor :3
+    func playMusic() {
+        guard let url = Bundle.main.url(forResource: "Your Preferred Spiciness for the Ultimate Choice", withExtension: "mp3") else {
+            print("No se encontró la canción en los Assets")
+            return
+        }
+        do {
+            player = try AVAudioPlayer(contentsOf: url)
+            player?.numberOfLoops = -1 // loop infinito
+            player?.play()
+        } catch {
+            print("Error al reproducir la canción: \\(error.localizedDescription)")
+        }
+    }
+    
 }
+
 
 #Preview {
     ContentView()
